842fc0c0e90d2d8a58d7a9f5e969dd2716c078ad
Kevin <77127847+KevinSmith2001@users.noreply.github.com>
2022-04-22 15:15:30 +0800

    New

---
+++ src/model/Pay.java
@@ -0,0 +1,95 @@
+package model;
+
+public class Pay {
+    private int payId;
+    private int registId;
+    private int pay_type;
+    private double price;
+    private String remark;
+    private String inputName;
+    private String inputDate;
+    private String updateName;
+
+    public int getPayId() {
+        return payId;
+    }
+
+    public void setPayId(int payId) {
+        this.payId = payId;
+    }
+
+    public int getRegistId() {
+        return registId;
+    }
+
+    public void setRegistId(int registId) {
+        this.registId = registId;
+    }
+
+    public int getPay_type() {
+        return pay_type;
+    }
+
+    public void setPay_type(int pay_type) {
+        this.pay_type = pay_type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getInputName() {
+        return inputName;
+    }
+
+    public void setInputName(String inputName) {
+        this.inputName = inputName;
+    }
+
+    public String getInputDate() {
+        return inputDate;
+    }
+
+    public void setInputDate(String inputDate) {
+        this.inputDate = inputDate;
+    }
+
+    public String getUpdateName() {
+        return updateName;
+    }
+
+    public void setUpdateName(String updateName) {
+        this.updateName = updateName;
+    }
+
+    public String getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public int getDelsign() {
+        return delsign;
+    }
+
+    public void setDelsign(int delsign) {
+        this.delsign = delsign;
+    }
+
+    private String updateDate;
+    private int delsign;
+}
