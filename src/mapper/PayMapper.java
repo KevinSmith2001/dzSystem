46b0ef129be332f4e5b7efdcd6bdde72db7c4427
Kevin <77127847+KevinSmith2001@users.noreply.github.com>
2022-04-22 16:19:08 +0800

    New1.4

---
+++ src/mapper/PayMapper.java
@@ -0,0 +1,8 @@
+package mapper;
+
+import model.Pay;
+
+public class PayMapper {
+    public Pay selectByRegistId(int registId) {
+    }
+}
--- src/servlet/student/PayServlet.java
+++ src/servlet/student/PayServlet.java
@@ -1,6 +1,8 @@
 package servlet.student;
-
-
+import mapper.PayMapper;
+import mapper.RegistMapper;
+import model.Pay;
+import model.Regist;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,11 +10,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-
 @WebServlet("/pay")
 public class PayServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        //衔接前端付款页面提交过来的信息，查询一下这个考生在之前是否缴费，如果没缴费，插入数据库
+        HttpSession session = request.getSession();
+        String idNumber = session.getAttribute("idNumber").toString();
+        Regist regist = new RegistMapper().selectByIdNumber(idNumber); //通过身份证号获取考生的全部信息进行存储
+        Pay pay = new PayMapper().selectByRegistId(regist.getRegistId());
     }
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
